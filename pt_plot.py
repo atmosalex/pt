bff4c6dd28fcba5eb8a1a0def43213ec7eb2bb13
alozinski@ucla.edu <alozinski@ucla.edu>
2025-02-13 09:39:42 -0600

    plotting code updated

--- pt_plot.py
+++ pt_plot.py
@@ -64,7 +64,30 @@ def interpolate_constant_dt(times, positions, dt_min=-1):
 
     return newtimes, newpositions, dt_min
 
-def plot_positions(positionslist, seeEarth=True, filename=None, limit=-1, view_ele = None, view_azi = None):
+def plot_positions(resultfile, ptids, seeEarth=True, filename=None, limit=-1, view_ele = None, view_azi = None, maxn=-1):
+    if maxn > 0:
+        nplot = min(len(ptids), maxn)
+    elif maxn == 0:
+        print("","warning, maxn set to zero, not plotting any trajectories...")
+        return
+    else:
+        nplot = len(ptids)
+
+    positionslist = []
+    nplotted = 0
+    for ptid in ptids:
+        if nplotted == nplot:
+            break
+        checkcode = ptids[ptid]
+        if checkcode != 1:
+            # checkcode > 1 could be caused by a number of issues, see return statements in solve_trajectory(...)
+            print("pt ID {} has incorrect check code".format(ptid))
+            continue
+
+        time, pos = resultfile.read_track(ptid, verbose = False)
+        positionslist.append(pos)
+
+
     # set up figure and axes:
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -179,7 +202,6 @@ def plot_positions2D_birdseye(resultfile, ptids, tracklist, seeEarth=True, filen
         ax1.add_patch(circle2)
 
 
-
     colours = colors(nplot)
 
     nplotted = 0
@@ -188,41 +210,9 @@ def plot_positions2D_birdseye(resultfile, ptids, tracklist, seeEarth=True, filen
             break
         checkcode = ptids[ptid]
 
-        # mu = tracklist[ptid][0]
-        # aeq = tracklist[ptid][1]
-        # L = tracklist[ptid][2]
-        # pg, pb, pd = tracklist[ptid][3:]
-
         if checkcode == 0: continue
 
         time, positions = resultfile.read_track(ptid, verbose = False, skipeveryn = skipeveryn)
-
-        # muenKalphaL0, muenKalphaL1 = resultfile.read_invariants(ptid)
-        # #print(muenKalphaL1)
-        # final_logmu = np.log10(muenKalphaL1[0] * mu_conv)
-        # final_aeq = muenKalphaL1[3]
-        # final_L = muenKalphaL1[4]
-        # final_iphase_g = muenKalphaL1[5]
-        # final_iphase_b = muenKalphaL1[6]
-        # final_iphase_d = muenKalphaL1[7]
-        # #print(final_logmu, final_aeq*180/np.pi, final_L, final_iphase_g, final_iphase_b, final_iphase_d)
-        # #print(muenKalphaL0[0]* mu_conv, muenKalphaL0[1:], muenKalphaL0[3]*180/pi)
-
-        # pre_pds.append(pd)
-        # pre_Ls.append(muenKalphaL0[4])
-        # pre_ens.append(muenKalphaL0[1])
-        # if checkcode == 1:
-        #     #print(muenKalphaL1[0]* mu_conv, muenKalphaL1[1:], muenKalphaL1[3]*180/pi)
-        #     post_Ls.append(muenKalphaL1[4])
-        #     post_ens.append(muenKalphaL1[1])
-        #     post_aeqs.append(muenKalphaL1[3])
-        #     post_Ks.append(muenKalphaL1[2])
-        # else:
-        #     print("","post-tracking invariants could not be evaluated")
-        #     post_Ls.append(-1)
-        #     post_ens.append(-1)
-        #     post_aeqs.append(-1)
-        #     post_Ks.append(-1)
 
 
         #draw particle trajectory:
@@ -251,7 +241,17 @@ def plot_positions2D_birdseye(resultfile, ptids, tracklist, seeEarth=True, filen
     plt.close()
     return [ax1.get_xlim(), ax1.get_ylim()]
 
-def plot_positions2D_side(positionslist, seeEarth=True, filename=None, ring = -1, axlims = []):
+
+
+def plot_positions2D_side(resultfile, ptids, tracklist, seeEarth=True, filename=None, ring = -1, axlims = [], maxn=-1, skipeveryn = 5):
+    if maxn > 0:
+        nplot = min(len(ptids), maxn)
+    elif maxn == 0:
+        print("","warning, maxn set to zero, not plotting any trajectories...")
+        return
+    else:
+        nplot = len(ptids)
+
     # set up figure and axes:
     fig, ax1 = plt.subplots()
 
@@ -263,15 +263,30 @@ def plot_positions2D_side(positionslist, seeEarth=True, filename=None, ring = -1
         ax1.add_patch(circle2)
 
 
-    colours = colors(len(positionslist))
+    colours = colors(nplot)
 
-    # draw particle trajectory:
 
-    colours = colors(len(positionslist))
-    for idx, positions in enumerate(positionslist):
+    nplotted = 0
+    for idx, ptid in enumerate(ptids):
+        if nplotted == nplot:
+            break
+        checkcode = ptids[ptid]
+
+        if checkcode == 0: continue
+
+        time, positions = resultfile.read_track(ptid, verbose = False, skipeveryn = skipeveryn)
+
+
+        #draw particle trajectory:
         positions = positions / pt_tools.constants.RE
-        ax1.plot(positions[:, 0], positions[:, 2], color=colours[idx], zorder=len(positionslist)-idx)
+        #ax1.scatter(positions[:, 0], positions[:, 1], color=colours[idx], marker = ".", zorder=1, s=0.1)
+        #ax1.plot(positions[:, 0], positions[:, 1], color=colours[idx], zorder=1, lw=0.1)
+        #ax1.scatter([positions[0, 0], positions[-1, 0]], [positions[0, 1], positions[-1, 1]], c=['black', 'red'], marker=".", zorder=2, s=1)
+
+        ax1.plot(positions[:, 0], positions[:, 2], color=colours[idx], zorder=len(ptids)-idx)
         ax1.scatter([positions[0, 0], positions[-1, 0]], [positions[0, 2], positions[-1, 2]], c=['black', 'red'],marker=".", zorder=1, s=1)
+
+        nplotted += 1
 
     # axis labels:
     plt.xlabel('$X_{\mathrm{MAG}}$ [$R_{\mathrm{E}}$]')
@@ -317,7 +332,7 @@ def plot_invariants(resultfile, ptids, tracklist, axes_invariants_idx = [4, 0, 7
         nplot = len(ptids)
 
     fig, ax = plt.subplots()
-    colormap = plt.cm.jet  # or any other colormap
+    colormap = plt.cm.hsv  # cyclic
 
     xyc0 = []
     xyc1 = []
@@ -381,17 +396,15 @@ def plot_invariants(resultfile, ptids, tracklist, axes_invariants_idx = [4, 0, 7
         x1, y1, c1 = xyc1[idx]
         arrowprops = dict(arrowstyle='<-', color=colormap(normalize(c0)), lw=0.75, ls='-')
         arrowprops = dict(arrowstyle='<-', color='black', lw=0.5, ls='-')
-        #ax.scatter([x0, x1],[y0, y1], c=[colormap(normalize(c)) for c in [c0,c1]], marker='.', zorder=1)
-
+        
         ax.scatter([x1], [y1], color=colormap(normalize(c1)), marker='.', zorder=1)
-        ax.annotate('', xy=(x0, y0),
-                    xycoords='data',
-                    xytext=(x1, y1),
-                    textcoords='data',
-                    arrowprops=arrowprops,
-                    zorder = 2)
-        #ax.scatter([x0], [y0], color='white', edgecolors='black', marker='.', zorder=3)
-        ax.scatter([x0], [y0], color=colormap(normalize(c0)), edgecolors='black', marker='.', zorder=3)
+        # ax.annotate('', xy=(x0, y0),
+        #             xycoords='data',
+        #             xytext=(x1, y1),
+        #             textcoords='data',
+        #             arrowprops=arrowprops,
+        #             zorder = 2)
+        ax.scatter([x0], [y0], color='black', edgecolors='black', marker='.', zorder=3)
 
 
     ax.set_xlabel(axes_invariants_labels[axes_invariants_idx[0]])
@@ -412,7 +425,176 @@ def plot_invariants(resultfile, ptids, tracklist, axes_invariants_idx = [4, 0, 7
     return [ax.get_xlim(), ax.get_ylim()]
 
 
+def plot_positions_multiplefiles(resultfiles, ptids_collection, seeEarth=True, filename=None, limit=-1, view_ele = None, view_azi = None, legend=False):
+    positionslist = []
+    for fileidx, ptids in enumerate(ptids_collection):
+        resultfile = resultfiles[fileidx]
+        for ptid in ptids:
+            checkcode = ptids[ptid]
+            if checkcode != 1:
+                # checkcode > 1 could be caused by a number of issues, see return statements in solve_trajectory(...)
+                print("pt ID {} has incorrect check code".format(ptid))
+                continue
 
+            time, pos = resultfile.read_track(ptid, verbose = False)
+            positionslist.append(pos)
+
+
+    # set up figure and axes:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    RE = pt_tools.constants.RE
+
+    if seeEarth:
+        from spacepy import coordinates as coord
+        from spacepy.time import Ticktock
+        import datetime
+        date= datetime.datetime(2015,1,1)
+
+        bfield = Dipolefield(date.year)
+        
+        x_ed_GEO_ = bfield.get_eccentric_centre_GEO()
+        #change them to the MAG frame:
+        x_ed_GEO = coord.Coords(x_ed_GEO_/RE, 'GEO', 'car')
+        x_ed_GEO.ticks = Ticktock(date, 'UTC')
+        x_ed_MAG = x_ed_GEO.convert('MAG', 'car')
+        #x_ed_MAG.ticks = Ticktock(epoch, 'UTC')
+        #
+        x_ed_GEO = RE * np.array([x_ed_GEO.x[0], x_ed_GEO.y[0], x_ed_GEO.z[0]])
+        x_ed_MAG = RE * np.array([x_ed_MAG.x[0], x_ed_MAG.y[0], x_ed_MAG.z[0]])
+
+        A = WGS84.M
+        A1 = A[:,0]
+        A2 = A[:,1]
+        A3 = A[:,2]
+        x_Earth_GEO = coord.Coords(np.array([A1,A2,A3]), 'GEO', 'car')
+        x_Earth_GEO.ticks = Ticktock(np.array([date]*3), 'UTC')
+        x_Earth_MAG = x_Earth_GEO.convert('MAG', 'car')
+        A = x_Earth_MAG.data
+
+        # rotate it
+        # https://math.stackexchange.com/questions/1403126/what-is-the-general-equation-equation-for-rotated-ellipsoid
+        ### find the rotation matrix and radii of the axes
+        U, s, rotation = linalg.svd(A)
+        radii = 1.0 / np.sqrt(s)
+
+        ue, ve = np.mgrid[0:2 * np.pi:24j, 0:np.pi:10j]
+
+
+        xe = radii[0] * np.cos(ue) * np.sin(ve) / constants.RE
+        ye = radii[1] * np.sin(ue) * np.sin(ve) / constants.RE
+        ze = radii[2] * np.cos(ve) / constants.RE
+
+
+        #add off-centre
+        xe += x_ed_MAG[0]/ constants.RE
+        ye += x_ed_MAG[1]/ constants.RE
+        ze += x_ed_MAG[2]/ constants.RE
+        ax.plot_wireframe(xe, ye, ze, color="deepskyblue", zorder=2)
+
+    # draw particle trajectory:
+    colours = colors(len(positionslist))
+    for idx, positions in enumerate(positionslist):
+        #if idx != 3: continue
+        positions = positions / RE
+        if limit > 0:
+            ax.plot3D(positions[:, 0][:limit], positions[:, 1][:limit], positions[:, 2][:limit], label=idx+1, color=colours[idx], zorder=1, linewidth=0.4)
+        else:
+            ax.plot3D(positions[:, 0], positions[:, 1], positions[:, 2], color=colours[idx], label=idx+1, zorder=1, linewidth=0.4)
+
+    # axis labels:
+    plt.xlabel('$X_{MAG}$ (RE)')
+    plt.ylabel('$Y_{MAG}$ (RE)')
+    ax.set_zlabel('$Z_{MAG}$ (RE)')
+    if legend: ax.legend()
+
+    limits = np.array([getattr(ax, f'get_{axis}lim')() for axis in 'xyz'])
+    ax.set_box_aspect(np.ptp(limits, axis = 1))
+    if (view_ele != None and view_azi != None):
+        ax.view_init(elev=view_ele, azim=view_azi)
+
+    # Get rid of colored axes planes
+    # First remove fill
+    ax.xaxis.pane.fill = False
+    ax.yaxis.pane.fill = False
+    ax.zaxis.pane.fill = False
+
+    # Now set color to white (or whatever is "invisible")
+    ax.xaxis.pane.set_edgecolor('w')
+    ax.yaxis.pane.set_edgecolor('w')
+    ax.zaxis.pane.set_edgecolor('w')
+    ax.grid(False)
+
+
+    plt.tight_layout()
+
+    if filename:
+        plt.savefig(filename, dpi=300, facecolor='w', edgecolor='w', orientation='portrait')
+    else:
+        plt.show()
+    plt.close()
+
+# b='pt_solutions/GC_reanalysis_test_GC_fwd_solutions.h5'
+# #b_re_approx10 = 'pt_solutions/GC_reanalysis_test_fwd_solutions_GC.h5'
+# b_re_approx5 = 'pt_solutions/GC_reanalysis_test_fwd_solutions_GC_skip.h5'
+# resultfiles = [pt_tools.HDF5_pt(b, existing=True),
+#     #pt_tools.HDF5_pt(b_re_approx10, existing=True),
+#     pt_tools.HDF5_pt(b_re_approx5, existing=True)]
+# ptids = [resultfile.get_solved_ids() for resultfile in resultfiles]
+# plot_positions_multiplefiles(resultfiles, ptids, seeEarth = False, view_ele = 0, view_azi = -71,legend=True,limit=100000);sys.exit()
+# sys.exit()
+
+
+
+# b='pt_solutions/GC_reanalysis_test_solutions.h5'
+# #b_re='pt_solutions/GC_reanalysis_test_solutions_GC_preserved_momentum.h5'
+# b_re_approx10 = 'pt_solutions/GC_reanalysis_test_solutions_GC_10.h5'
+# b_re_approx5 = 'pt_solutions/GC_reanalysis_test_solutions_GC_5.h5'
+# resultfiles = [pt_tools.HDF5_pt(b, existing=True),
+#     #pt_tools.HDF5_pt(b_re, existing=True),
+#     pt_tools.HDF5_pt(b_re_approx10, existing=True),
+#     pt_tools.HDF5_pt(b_re_approx5, existing=True)]
+# ptids = [resultfile.get_solved_ids() for resultfile in resultfiles]
+# plot_positions_multiplefiles(resultfiles, ptids, seeEarth = False, view_ele = 0, view_azi = -71,legend=True,limit=100000);sys.exit()
+# sys.exit()
+
+# d='pt_solutions/GC_reanalysis_test_GC_solutions.h5'
+# d_re='pt_solutions/GC_reanalysis_test_solutions_GC.h5'
+# resultfiles = [pt_tools.HDF5_pt(d, existing=True), pt_tools.HDF5_pt(d_re, existing=True)]
+# ptids = [resultfile.get_solved_ids() for resultfile in resultfiles]
+# plot_positions_multiplefiles(resultfiles, ptids, seeEarth = False, view_ele = 0, view_azi = -71,legend=True);sys.exit()
+# sys.exit()
+# b='pt_solutions/GC_reanalysis_test_GC_fwd_solutions.h5'
+# b_re='pt_solutions/GC_reanalysis_test_fwd_solutions_GC.h5'
+# resultfiles = [pt_tools.HDF5_pt(b, existing=True), pt_tools.HDF5_pt(b_re, existing=True)]
+# ptids = [resultfile.get_solved_ids() for resultfile in resultfiles]
+# plot_positions_multiplefiles(resultfiles, ptids, seeEarth = False, view_ele = 0, view_azi = -71,legend=True);sys.exit()
+# sys.exit()
+
+# x='pt_solutions/GC_reanalysis_test_GC_fwd_solutions.h5'
+# y='pt_solutions/GC_reanalysis_test_GC_solutions.h5'
+# resultfiles = [pt_tools.HDF5_pt(x, existing=True), pt_tools.HDF5_pt(y, existing=True)]
+# ptids = [resultfile.get_solved_ids() for resultfile in resultfiles]
+# plot_positions_multiplefiles(resultfiles, ptids, seeEarth = False, view_ele = 0, view_azi = -71)
+# sys.exit()
+
+# c='pt_solutions/GC_reanalysis_test_solutions.h5'
+# d='pt_solutions/GC_reanalysis_test_GC_solutions.h5'
+# resultfiles = [pt_tools.HDF5_pt(c, existing=True), pt_tools.HDF5_pt(d, existing=True)]
+# ptids = [resultfile.get_solved_ids() for resultfile in resultfiles]
+# #plot_positions_multiplefiles(resultfiles, ptids, seeEarth = False, view_ele = 0, view_azi = -71);sys.exit()
+# for ptid in ptids[-1]:
+#     _, GC1 = resultfiles[-1].read_track(ptid, verbose = False)
+# np.save('GC1.npy', GC1)
+# a='pt_solutions/GC_reanalysis_test_fwd_solutions.h5'
+# b='pt_solutions/GC_reanalysis_test_GC_fwd_solutions.h5'
+# resultfiles = [pt_tools.HDF5_pt(a, existing=True), pt_tools.HDF5_pt(b, existing=True)]
+# ptids = [resultfile.get_solved_ids() for resultfile in resultfiles]
+# #plot_positions_multiplefiles(resultfiles, ptids, seeEarth = False, view_ele = 0, view_azi = -71);sys.exit()
+# for ptid in ptids[-1]:
+#     _, GC2 = resultfiles[-1].read_track(ptid, verbose = False)
+# np.save('GC2.npy', GC2)
+# sys.exit()
 
 
 #set up parser and arguments: ------------------------------------------------+
@@ -423,7 +605,9 @@ parser.add_argument("--solution",type=str, required=True)
 args = parser.parse_args()
 
 fileh5 = args.solution
+dirname = os.path.dirname(fileh5)
 #-----------------------------------------------------------------------------+
+
 
 
 resultfile = pt_tools.HDF5_pt(fileh5, existing=True)
@@ -462,32 +646,37 @@ tracklist = resultfile.get_existing_tracklist()
 plotname = os.path.basename(fileh5[:-3])
 maxn = -1
 
+# print("Printing 3D overview, click and drag to move around...")
+# plot_positions(resultfile, ptids, seeEarth = False, view_ele = 0, view_azi = -71)
+# print()
+# print("quitting...")
+# sys.exit()
+
+
 print("Plotting changes to invariants...")
-axlims = plot_invariants(resultfile, ptids, tracklist, filename="Figure_adiabatics_{}.png".format(plotname),
+axlims = plot_invariants(resultfile, ptids, tracklist, filename=os.path.join(dirname,"{}_Figure_adiabatics.png".format(plotname)),
     maxn=maxn,
     axlims = [],
     )#axes_invariants_idx = [7,4,1])
 #flc:
 #axlims = plot_invariants(filename="Figure_adiabatics_{}.png".format(plotname), maxn=maxn,
 #    axlims = [], axes_invariants_idx = [1, 2, 3])
-
-#print(axlims)
+print(axlims)
 print()
+print("quitting...")
 
-#print("Printing 3D overview, click and drag to move around...")
-#plot_positions(seeEarth = False, view_ele = 0, view_azi = -71)
-#print()
+
 
 print("Printing 2D overview looking down towards Earth...")
-axlims = plot_positions2D_birdseye(resultfile, ptids, tracklist, seeEarth = False, ring = -1, filename="Figure_birdseye_{}.png".format(plotname),
+axlims = plot_positions2D_birdseye(resultfile, ptids, tracklist, seeEarth = False, ring = -1, skipeveryn=5, filename=os.path.join(dirname,"{}_Figure_birdseye.png".format(plotname)),
     maxn=maxn,
-    axlims=[],
-    skipeveryn=25)
+    axlims=[])
 print(axlims)
 print()
 
-#print("Printing 2D overview side-on...")
-#axlims = plot_positions2D_side(seeEarth = False, ring = -1, filename="Figure_side_{}.png".format(plotname),
-#    axlims=[])
-#print(axlims)
-#print()
+print("Printing 2D overview side-on...")
+axlims = plot_positions2D_side(resultfile, ptids, tracklist, seeEarth = False, ring = -1, skipeveryn=5, filename=os.path.join(dirname,"{}_Figure_side.png".format(plotname)),
+    maxn=maxn,
+    axlims=[])
+print(axlims)
+print()
